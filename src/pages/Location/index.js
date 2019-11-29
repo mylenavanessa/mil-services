17499b857965dc95bb3e6c4c5e2e63611f6b6ae7
mylenavanessa <mylenavanessa@hotmail.com>
2019-11-29 19:50:23 -0300

    implement register

--- src/pages/Location/index.js
+++ src/pages/Location/index.js
@@ -1,30 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Image, Text, TextInput, Button } from 'react-native';
 
 import styles from './styles';
 
 export default function SignUp({ navigation }) {
+  const [zipCode, setZipCode] = useState('')
+  const [street, setStreet] = useState('')
+  const [district, setDistrict] = useState('')
+  const [complement, setComplement] = useState('')
+  const [number, setNumber] = useState('')
+
   return (
     <View style={styles.container}>
    <Image resizeMode="contain" source={require('../../assets/logo.png')} style={styles.image}/>
    <Text  style={styles.text}>que tal você colocar sua localização?</Text>
    <View style={styles.viewInput}>
       <TextInput
-        style={styles.textInput} placeholder= 'CEP'
+        style={styles.textInput} 
+        placeholder= 'CEP'
+        autoCapitalize='none'
+        autoCorrect={false}
+        value={zipCode}
+        onChangeText={setZipCode}
       />
       <TextInput
-        style={styles.textInput} placeholder= 'Rua'
+        style={styles.textInput} 
+        placeholder= 'Rua'
+        autoCapitalize='none'
+        autoCorrect={false}
+        value={street}
+        onChangeText={setStreet}
       />
       <TextInput
-        style={styles.textInput} placeholder= 'Bairro'
+        style={styles.textInput} 
+        placeholder= 'Bairro'
+        autoCapitalize='none'
+        autoCorrect={false}
+        value={district}
+        onChangeText={setDistrict}
       />
       <TextInput
-        style={styles.textInput} placeholder= 'Complemento'
+        style={styles.textInput} 
+        placeholder= 'Complemento'
+        autoCapitalize='none'
+        autoCorrect={false}
+        value={complement}
+        onChangeText={setComplement}
       />
-      <TextInput style={styles.numero} placeholder= 'Número'
+      <TextInput 
+        style={styles.numero} 
+        placeholder= 'Número'
+        autoCapitalize='none'
+        autoCorrect={false}
+        value={number}
+        onChangeText={setNumber}
       />
       </View>
-      <Button style={styles.button} title= 'Confirmar Endereço' onPress={() => navigation.navigate('SignUp')}/>
+      <Button 
+        style={styles.button} 
+        title= 'Confirmar Endereço' 
+        onPress={() => navigation.navigate('SignUp', {
+          zipCode,
+          street,
+          district,
+          complement,
+          number
+        })}
+
+        />
     </View>
   );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,35 +1,89 @@
-import React from 'react';
-import { View, Image, TextInput, Text, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { View, Image, TextInput, Text, TouchableOpacity, Alert } from 'react-native';
 
+import api from '../../services/api'
 import style from './styles';
 
 export default function Location({ navigation }) {
+
+  const [name, setName] = useState('')
+  const [cpf, setCpf] = useState('')
+  const [phone, setPhone] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  async function signUp() {
+    const address =  {
+      'zip_code': navigation.getParam('zipCode'),
+      street: navigation.getParam('street'),
+      district: navigation.getParam('district'),
+      complement: navigation.getParam('complement'),
+      number: navigation.getParam('number')
+    }
+
+    const { data } = await api.post('/clients', {
+      name,
+      cpf,
+      phone,
+      email,
+      password,
+      address
+    })
+
+    if (data._id) {
+      navigation.navigate('Profile')
+    }
+    else{
+      Alert.alert('Erro ao registrar, tente novamente')
+    }
+  } 
+
   return (
     <View style={style.container}>
      <Image resizeMode="contain" source={require('../../assets/logo.png')} style={style.image}/>
      <TextInput
         style={style.textInput}
         placeholder= 'Nome completo'
+        autoCapitalize='none'
+        autoCorrect={false}
+        value={name}
+        onChangeText={setName}
       />
       <TextInput
         style={style.textInput}
         placeholder= 'CPF'
+        autoCapitalize='none'
+        autoCorrect={false}
+        value={cpf}
+        onChangeText={setCpf}
       />
       <TextInput
         style={style.textInput}
         placeholder= 'Celular'
+        autoCapitalize='none'
+        autoCorrect={false}
+        value={phone}
+        onChangeText={setPhone}
       />
       <TextInput
         style={style.textInput}
         placeholder= 'E-mail'
+        keyboardType= 'email-address'
+        autoCapitalize='none'
+        autoCorrect={false}
+        value={email}
+        onChangeText={setEmail}
       />
       <TextInput
         style={style.textInput}
         secureTextEntry={true}
         placeholder= 'Senha'
+        keyboardType= 'visible-password'
+        value={password}
+        onChangeText={setPassword}
       />
 
-      <TouchableOpacity style={style.button}>
+      <TouchableOpacity style={style.button} onPress={signUp}>
         <Text style={style.textButton}>Registrar</Text>
       </TouchableOpacity>
 
