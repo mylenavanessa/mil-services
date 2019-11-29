a64c8a52f3bf04ab1fd104f80605ae4750c5ae4a
mylenavanessa <mylenavanessa@hotmail.com>
2019-11-29 02:39:37 -0300

    location page

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,12 @@ import { createStackNavigator } from 'react-navigation-stack'
 
 import Login from '../pages/Login'
 import SignUp from '../pages/SignUp'
+import Location from '../pages/Location'
 
 const Router = createStackNavigator({
   Login, 
-  SignUp
+  SignUp,
+  Location
 })
 
 export default createAppContainer(Router)
